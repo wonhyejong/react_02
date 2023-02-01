2dc358968f78aa26b9a0a7af08a24beb10cb7170
wonhj <wonhjteacher@gmail.com>
2023-02-01 15:11:17 +0900

    add pageNum styled

--- src/components/Pagenation.jsx
+++ src/components/Pagenation.jsx
@@ -1,23 +1,63 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Pagenation = ({total,page,setPage,LIST_PER_PAGE}) => {
     const pageNum = Math.ceil(total/LIST_PER_PAGE)
     const pageNumArray = new Array(pageNum);
     return (
-        <div>
-            <button onClick={()=>setPage(page-1)} disabled={page===1}>이전</button>
+        <PaginationBox>
+            <PrevButton onClick={()=>setPage(page-1)} disabled={page===1}>이전</ PrevButton>
             {
                 pageNumArray.fill().map((ele,idx)=>{
                     return(
-                        <button key={idx} onClick={()=>setPage(idx+1)}>
+                        <PageNumButton 
+                            key={idx} 
+                            onClick={()=>setPage(idx+1)} 
+                            className={page===idx+1 && "active"}
+                        >
                             {idx+1}
-                        </button>
+                        </PageNumButton>
                     )
                 }) 
             }
-            <button onClick={()=>setPage(page+1)} disabled={page === pageNum} >이후</button>
-        </div>
+            <NextButton onClick={()=>setPage(page+1)} disabled={page === pageNum} >이후</NextButton>
+        </PaginationBox>
     );
 };
 
 export default Pagenation;
+const PaginationBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width:1000px;
+    margin:1rem auto;
+`
+const PrevButton = styled.button`   
+    border:none;
+    border-radius: 0.25rem;
+    padding:0.25rem 0.375rem;
+    color:#fff;
+    width:40px;height:24px;
+    background:#cda8a8;
+    font-size: 0.75rem;
+    &:hover{
+        cursor: pointer;
+        color:#f23;
+    }
+    &[disabled]{
+        background:#e0e0e0;
+        color:#fff;
+    }
+    &.active{
+        font-weight: bold;
+        color:#f93;
+        text-decoration:underline;
+    }
+`
+const NextButton  = styled(PrevButton)``
+const PageNumButton = styled(PrevButton)`
+    background:none;
+    color:#666;
+    font-size: 1rem;
+`
